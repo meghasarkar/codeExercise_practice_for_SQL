6d0ce99c2247340864851ca8f18bc519f2bba14f
Megha Sarkar <meghasarkar64@gmail.com>
2022-03-11 13:23:12 +0530

    Issue fixed for average rating and masked the password for db connection

--- internal/src/hub.py
+++ internal/src/hub.py
@@ -1,18 +1,18 @@
 import psycopg2
 
-"""Parameterized the value for output table name 'supplier_score_metrics'"""
+"""Parameterized the value for output table name 'supplier_score_metrics'. """
 outTable = 'supplier_score_metrics'
 
-"""Connect to database server to run create table statement"""
+"""Connect to database server to run create table statement. Change the password value """
 connect2DB = psycopg2.connect(dbname='practiceDB', host='localhost', port='5432', user='postgres',
                               password='$$$$$$$')
 cur2DB = connect2DB.cursor()
 
 """Create a New table for dumping the metrics table"""
 createTableStatement = 'CREATE TABLE IF NOT EXISTS ' + outTable + '( calculated_at DATE DEFAULT CURRENT_TIMESTAMP,' \
-                       'supplier_id INT NOT NULL,' \
-                       'metrics VARCHAR, ' \
-                       'values VARCHAR );'
+                                                                  'supplier_id INT NOT NULL,' \
+                                                                  'metrics VARCHAR, ' \
+                                                                  'values VARCHAR );'
 cur2DB.execute(createTableStatement)
 connect2DB.commit()
 
@@ -25,13 +25,14 @@ cur2DB.execute(accepted_supplierID)
 list_supplierID = cur2DB.fetchall()
 
 for record in list_supplierID:
+    """The rate of ratio calculation where acceptance_count gives total count of payment for a particular hub_id 
+    and acceptance_rate looks for count by total order submitted"""
     acceptance_count = "SELECT count(data -> 'event') FROM \"MY_TABLE\" " \
                        "WHERE name = 'order/execute/customer/status/payment' and cast(data->>'hub_id' as integer) = " + \
                        record[0]
     cur2DB.execute(acceptance_count)
     ((acceptance_count_result,),) = cur2DB.fetchall()
 
-    """The rate of ratio calculation"""
     acceptance_rate = "SELECT round( " + str(acceptance_count_result) + " " + " * 100.0/(select count(data -> 'event') " \
                                                                               "FROM \"MY_TABLE\" where name = " \
                                                                               "'order/execute/customer/status" \
@@ -39,6 +40,15 @@ for record in list_supplierID:
     cur2DB.execute(acceptance_rate)
     ((acceptance_rate_result,),) = cur2DB.fetchall()
     percent_acceptance = str(acceptance_rate_result) + '%'
+
+    """Query for listing the rate ratio by adding 2 terms in data review_value_speed and review_value_print_quality to 
+    the average"""
+    review_rate = "SELECT round(avg(cast(data->> 'review_value_speed'as integer)+(cast(data->> " \
+                  "'review_value_print_quality'as integer)))/2) FROM \"MY_TABLE\" WHERE name = " \
+                  "'node/review/created' and cast(data->>'hub_id' as integer) = " + record[0]
+    cur2DB.execute(review_rate)
+    ((review_rate_result,),) = cur2DB.fetchall()
+    percent_review = str(review_rate_result)
 
     """The current date SELECT Query"""
     calculated_at = "SELECT CURRENT_DATE"
@@ -50,7 +60,7 @@ for record in list_supplierID:
                           + "' , '" + percent_acceptance + "' )"
             cur2DB.execute(dump_output)
         else:
-            dump_output = "INSERT INTO " + outTable + " VALUES (( " + calculated_at + "), " + record[0] + ", '"\
-                                                              + metric + "' , 77 " + ")"
+            dump_output = "INSERT INTO " + outTable + " VALUES (( " + calculated_at + "), " + record[0] + ", '" + metric\
+                          + "' , '" + percent_review + "' )"
             cur2DB.execute(dump_output)
             connect2DB.commit()
