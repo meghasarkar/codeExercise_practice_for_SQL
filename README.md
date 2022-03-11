c070690eedeafe186512f4aadb42200df0b52152
Megha Sarkar <meghasarkar64@gmail.com>
2022-03-11 12:59:55 +0530

    GIt commit initial of Hubs exercise

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# codeExercise_MeghaSarkar_Hubs
+
+# Code Exercise for Hubs
+The test exercise for technical evaluation
+Data Engineer - Hubs
+
+
+
+
+
+## Prerequisites
+### Python 3.6.* or later.
+
+See installation instructions at: https://www.python.org/downloads/
+
+Check you have python3 installed:
+
+```bash
+python3 --version
+```
+
+### Preferably an IDE such as Pycharm Community Edition
+
+https://www.jetbrains.com/pycharm/download/
+
+### Postgres SQL 
+
+Install postgres on local or EC2 machine for loading and execution of data and logic file.
+
+```bash
+postgres -V postgres (PostgreSQL) 9.3.10
+```
+
+## Installing Python requirements
+
+This will install some of the packages you might find useful:
+
+```bash
+pip3 install -r ./external/requirements.txt
+```
+
+## Loading the data to Postgres
+
+1. Open pgAdmin 4 and connect to local or remote database(as per your requirements)
+2. Load the hubs_event.sql file from the import file available in the application.
+3. Execute the query file and check for the loaded data by executing simple query of 
+```bash
+select * from "MY_TABLE";
+```
+
+## Execution of program from Pycharm
+1. Start Pycharm application.
+2. CLick on Open project and load the folder from *./hubs*
+3. After the project is loaded, traverse to the main py file named hubs.py from *.\hubs\internal\src*
+4. Run the application and wait for the response code to be 0 (No error)
+5. Check for the output file from pgAdmin by quering
+```bash
+select * from "supplier_score_metrics";
+```
+
+
+## Author 
+
+[@meghasarkar](https://www.linkedin.com/in/megha-sarkar-1c9/)
+
+
+
+## Note:
+
+Each function and flow of the program is explained in the *hubs.py* file itself for better illustration of content.
+
---
+++ internal/src/hub.py
@@ -0,0 +1,56 @@
+import psycopg2
+
+"""Parameterized the value for output table name 'supplier_score_metrics'"""
+outTable = 'supplier_score_metrics'
+
+"""Connect to database server to run create table statement"""
+connect2DB = psycopg2.connect(dbname='practiceDB', host='localhost', port='5432', user='postgres',
+                              password='$$$$$$$')
+cur2DB = connect2DB.cursor()
+
+"""Create a New table for dumping the metrics table"""
+createTableStatement = 'CREATE TABLE IF NOT EXISTS ' + outTable + '( calculated_at DATE DEFAULT CURRENT_TIMESTAMP,' \
+                       'supplier_id INT NOT NULL,' \
+                       'metrics VARCHAR, ' \
+                       'values VARCHAR );'
+cur2DB.execute(createTableStatement)
+connect2DB.commit()
+
+"""The SELECT command for the query to find the Supplier ID (hub_id) who accepted order 
+(event = 'order/execute/customer/status/payment') """
+accepted_supplierID = "SELECT data -> 'hub_id' FROM \"MY_TABLE\" WHERE name = 'order/execute/customer/status/payment'" \
+                      " group by data -> 'hub_id'"
+
+cur2DB.execute(accepted_supplierID)
+list_supplierID = cur2DB.fetchall()
+
+for record in list_supplierID:
+    acceptance_count = "SELECT count(data -> 'event') FROM \"MY_TABLE\" " \
+                       "WHERE name = 'order/execute/customer/status/payment' and cast(data->>'hub_id' as integer) = " + \
+                       record[0]
+    cur2DB.execute(acceptance_count)
+    ((acceptance_count_result,),) = cur2DB.fetchall()
+
+    """The rate of ratio calculation"""
+    acceptance_rate = "SELECT round( " + str(acceptance_count_result) + " " + " * 100.0/(select count(data -> 'event') " \
+                                                                              "FROM \"MY_TABLE\" where name = " \
+                                                                              "'order/execute/customer/status" \
+                                                                              "/processing'))"
+    cur2DB.execute(acceptance_rate)
+    ((acceptance_rate_result,),) = cur2DB.fetchall()
+    percent_acceptance = str(acceptance_rate_result) + '%'
+
+    """The current date SELECT Query"""
+    calculated_at = "SELECT CURRENT_DATE"
+
+    """Query for committing the values into the TABLE test"""
+    for metric in ('acceptance_ratio', 'average_rating'):
+        if metric == 'acceptance_ratio':
+            dump_output = "INSERT INTO " + outTable + " VALUES (( " + calculated_at + "), " + record[0] + ", '" + metric \
+                          + "' , '" + percent_acceptance + "' )"
+            cur2DB.execute(dump_output)
+        else:
+            dump_output = "INSERT INTO " + outTable + " VALUES (( " + calculated_at + "), " + record[0] + ", '"\
+                                                              + metric + "' , 77 " + ")"
+            cur2DB.execute(dump_output)
+            connect2DB.commit()
